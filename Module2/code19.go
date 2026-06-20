270e2306ddccb9c0b4f49eeeb6d27531d355c229
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-06-21 01:27:41 +0530

    go learning commit

---
+++ Module2/code19.go
@@ -0,0 +1,9 @@
+package main 
+import "fmt"
+ func main(){
+	n,err:=fmt.Println("sp")
+	fmt.Println("Bytes written:",n) //Output = 3 because "sp"=> 's','p','\n'
+	fmt.Println("Error:",err)
+
+	//S
+ }
