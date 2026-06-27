528e0b3e59198cbc1c4aed5a43d4c7705d77dc5f
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-06-27 23:57:30 +0530

    Practised go day2

--- Module1/Practise.go
+++ Module1/Practise.go
@@ -1,6 +1,13 @@
-package main 
+package main
+
 import "fmt"
-func main(){
-	name:="lisa"
-	fmt.Println(name)
+
+func main() {
+    arr := [4]int{10, 20, 30, 40}
+    i:=2//element that you want to delete
+		fmt.Println("Original array:",arr)
+		slice_1:=arr[:i]
+		slice_2:=arr[i+1:]
+		finalSlice:=append(slice_1,slice_2...)
+		fmt.Println(finalSlice)
 }
