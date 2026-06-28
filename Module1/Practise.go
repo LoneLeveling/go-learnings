87f9bbe5f7d6baa29bc5b6d21d9d88afa277082c
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-06-28 22:40:18 +0530

    Day28 of go practise

--- Module1/Practise.go
+++ Module1/Practise.go
@@ -2,12 +2,39 @@ package main
 
 import "fmt"
 
-func main() {
-    arr := [4]int{10, 20, 30, 40}
-    i:=2//element that you want to delete
-		fmt.Println("Original array:",arr)
-		slice_1:=arr[:i]
-		slice_2:=arr[i+1:]
-		finalSlice:=append(slice_1,slice_2...)
-		fmt.Println(finalSlice)
+func calcArea(r float64) float64{
+	return 3.14*r*r
+}
+
+func calcPerimeter(r float64) float64{
+	return 2*3.14*r
+}
+
+func calcDiameter(r float64) float64{
+	return 2*r
+}
+
+func printResult(radius float64, calcFunction func(r float64) float64){
+	result:=calcFunction(radius)
+	fmt.Println("Result: ", result)
+	fmt.Println("Thank you!")
+}
+
+func getFunction(query int) func (r float64) float64{ //"getFunction takes an integer and returns another function that takes a float64 and returns a float64."
+	query_to_func:=map[int]func(r float64)float64{
+		1:calcArea,
+		2:calcPerimeter,
+		3:calcDiameter,
+	}
+	return query_to_func[query]
+}
+func main(){
+var query int
+var radius float64
+fmt.Println("Enter the radius of the circle: ")
+fmt.Scanf("%f\n",&radius)
+fmt.Println("Enter \n 1 - area \n 2-Perimeter \n 3-Diameter")
+fmt.Scanf("%d\n",&query)
+
+printResult(radius, getFunction(query))
 }
