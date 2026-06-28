7c4a6352c88f1ba889389eb47bf58130944ad20e
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-06-29 00:39:54 +0530

    Added go.mod

--- Practise/Practise.go
+++ Practise/Practise.go
@@ -1,12 +1,31 @@
-package main 
+package main
+
 import "fmt"
-type Student struct{
-name string
-rollNo in
-marks []int
-grades map[string] int
+
+type Circle struct {
+	x      int
+	y      int
+	radius float64
+	area   float64
 }
 
-func main(){
+func main() {
+	c := Circle{x: 10, y: 20, radius: 80, area: 0}
 
+	fmt.Printf("%+v\n", c)
+
+	calcArea(&c)
+
+	fmt.Printf("%+v\n", c)
+}
+
+func calcArea(c *Circle) {
+	var area float64
+
+	area = float64(c.x) * float64(c.y) * c.radius
+
+	c.area = area // same as (*c).area = area
+
+	// or:
+	// (*c).area = area
 }
